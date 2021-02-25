9f47480811b60fba3bdb3f31e782c84f893552fc
tamyressilvazz <74621120+tamyressilvazz@users.noreply.github.com>
2021-02-25 11:18:41 -0300

    Update q7.py

--- q7.py
+++ q7.py
@@ -2,11 +2,11 @@
 Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 '''
 
-letra = input("Digite uma letra: ")
+letra = input("Digite uma letra: ") #guarda a letra digitada em letra
 
-vogais = 'aeiou'
+vogais = 'aeiou'                    #string com as vogais
 
-if letra in vogais or letra in vogais.upper():
-    print(f"A letra {letra} é uma VOGAL!")
-else:
+if letra in vogais or letra in vogais.upper():      #verifica se a letra está em vogais minuscula OU vogais maiusculas
+    print(f"A letra {letra} é uma VOGAL!")         
+else:                                               #se não for uma vogal, consequentemente é uma consoante
     print(f"A letra {letra} é uma CONSOANTE")
