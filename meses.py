22bfd88e32a9a4648ec27b6899739241b5fbf2f3
tamyressilvazz <74621120+tamyressilvazz@users.noreply.github.com>
2021-03-04 22:43:40 -0300

    Add files via upload

---
+++ meses.py
@@ -0,0 +1,10 @@
+'''
+Escreva um programa que usa um laço for para imprimir
+Um dos meses do ano é janeiro
+Um dos meses do ano é fevereiro
+Um dos meses do ano é março
+etc …
+'''
+mes = ['janeiro','fevereiro','março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+for meses in mes:
+    print(f"Um dos meses do ano é {meses}")
