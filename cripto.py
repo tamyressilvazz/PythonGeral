25a7541b264f0f40eb4d67407a7fcf4c35983585
tamyressilvazz <74621120+tamyressilvazz@users.noreply.github.com>
2021-03-15 01:47:58 -0300

    Add files via upload
    criptografa uma frase

---
+++ cripto.py
@@ -0,0 +1,13 @@
+#Criptografando uma frase/texto
+texto = input("Digite algo: ")
+cifra = ''
+for char in texto:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char) + 1
+    if code > ord('Z'):
+        code = ord('A')
+    cifra += chr(code)
+
+print(cifra)
