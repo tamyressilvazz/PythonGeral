7995cb9aaf290a620ebebdfb31b14a9bc34a7434
tamyressilvazz <74621120+tamyressilvazz@users.noreply.github.com>
2021-03-27 20:56:38 -0300

    dobro, triplo e raiz quadrada

---
+++ Desafio06.py
@@ -0,0 +1,15 @@
+import math
+
+print("==========Desafio 06==========")
+print("--------DOBRO, TRIPLO E RAIZ QUADRADA--------")
+
+numero = float(input("Número: "))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz_Quad = math.sqrt(numero)
+
+print(f"\tO dobro: {dobro}")
+print(f"\tO triplo: {triplo}")
+print(f"\tA raiz quadrada: {raiz_Quad}")
+
